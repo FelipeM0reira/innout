dfcdc8184e6b418a50ab297a725e7829deeebabb
Felipe Moreira <104744113+FelipeM0reira@users.noreply.github.com>
2023-07-13 21:51:03 -0300

    :construction: feat: Models e Users.

---
+++ src/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+date_default_timezone_set(' America/Belem');
+setlocale(LC_TIME, 'pt_BR', 'pt_BR.uft-8', 'portuguese');
+
+// Pastas
+define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
+
+require_once(realpath(dirname(__FILE__) . '/database.php'));
