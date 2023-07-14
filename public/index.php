e04870c0859a2959262954f48bb4e7d4ab81c15d
Felipe Moreira <104744113+FelipeM0reira@users.noreply.github.com>
2023-07-14 15:18:15 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,4 +7,4 @@ $sql = 'select * from users';
 $result = Database::getResultFromQuery($sql);
 
 $user = new User(['name' => 'Lucas', 'email' => 'lucascod3r.com.br']);
-print_r($user->name);
+echo $user->getSelect('id, name');
