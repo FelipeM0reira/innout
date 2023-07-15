fd881ffb6c7b95f4365ec83efcad628c402bd199
Felipe Moreira <104744113+FelipeM0reira@users.noreply.github.com>
2023-07-15 11:57:45 -0300

    Update: Obter usuário do banco.

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
-$user = new User(['name' => 'Lucas', 'email' => 'lucascod3r.com.br']);
-// echo $user->getSelect();
-
-echo User::getSelect(['name' => 'Chaves'], 'name, email');
+foreach(User::get([], 'name') as $user) {
+  echo $user->name;
+  echo '<br>';
+}
