4c0f9140e507a7ea9200506af339a500e823c6da
Felipe Moreira <104744113+FelipeM0reira@users.noreply.github.com>
2023-07-17 15:19:55 -0300

    Update: Login #02.

--- public/index.php
+++ public/index.php
@@ -1,18 +1,20 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-// require_once(VIEW_PATH . '/login.php');
+require_once(CONTROLLER_PATH . '/login.php');
 
-require_once(MODEL_PATH . '/Login.php');
+// loadView('login', ['texto' => 'abc123']);
 
-$login = new Login([
-  'email' => 'quico@cod3r.com.br',
-  'password' => 'a'
-]);
+// require_once(MODEL_PATH . '/Login.php');
 
-try {
-  $login->checkLogin();
-  echo 'Deu certo :)';
-} catch (Exception $e) {
-  echo 'Problema no login :P';
-}
+// $login = new Login([
+//   'email' => 'quico@cod3r.com.br',
+//   'password' => 'a'
+// ]);
+
+// try {
+//   $login->checkLogin();
+//   echo 'Deu certo :)';
+// } catch (Exception $e) {
+//   echo 'Problema no login :P';
+// }
--- src/views/login.php
+++ src/views/login.php
@@ -24,7 +24,8 @@
       <div class="card-body">
         <div class="form-group">
           <label for="email">E-mail</label>
-          <input type="email" id="email" name="email" class="form-control" placeholder="Informe o e-mail" autofocus>
+          <input type="email" id="email" name="email" class="form-control" value="<?= $email ?>"
+            placeholder="Informe o e-mail" autofocus>
         </div>
         <div class="form-group">
           <label for="password">Senha</label>
