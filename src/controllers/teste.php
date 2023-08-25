7347a847c4cc11f22c62f1f946aa3a5f726df208
Felipe Moreira <104744113+FelipeM0reira@users.noreply.github.com>
2023-08-25 16:05:55 -0300

    :construction: feat: Calculo do intervalo do almoço.

--- src/controllers/teste.php
+++ src/controllers/teste.php
@@ -8,3 +8,7 @@ $wh = WorkingHours::loadFromUserAndDate(2, date('Y-m-d'));
 $workIntervalString = $wh->getWorkedInterval()->format('%H:%I:%S');
 print_r($workIntervalString);
 echo '<br>';
+
+$lunchIntervalString = $wh->getLunchInterval()->format('%H:%I:%S');
+print_r($lunchIntervalString);
+echo '<br>';
