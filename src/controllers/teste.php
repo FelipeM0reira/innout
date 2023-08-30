0b715f4137314c00e4686dd6b69f688d0184ca8d
Felipe Moreira <104744113+FelipeM0reira@users.noreply.github.com>
2023-08-30 16:56:38 -0300

    :construction: feat: Relatório mensal.

--- src/controllers/teste.php
+++ src/controllers/teste.php
@@ -1,16 +1,2 @@
 <?php 
 // Controller temporario
-
-loadModel('WorkingHours');
-
-$wh = WorkingHours::loadFromUserAndDate(2, date('Y-m-d'));
-
-$workIntervalString = $wh->getWorkedInterval()->format('%H:%I:%S');
-print_r($workIntervalString);
-echo '<br>';
-
-$lunchIntervalString = $wh->getLunchInterval()->format('%H:%I:%S');
-print_r($lunchIntervalString);
-echo '<br>';
-
-print_r($wh->getExitTime());
