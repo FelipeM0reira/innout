dfc407a4732a4e424fa3e23459169d992c3a2553
Felipe Moreira <104744113+FelipeM0reira@users.noreply.github.com>
2023-08-30 01:28:34 -0300

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5,3 +5,19 @@
     body.classList.toggle('hide-sidebar')
   }
 })()
+
+function addOneSecond(hours, minutes, seconds) {
+  const d = new Date()
+  d.setHours(parseInt(hours))
+  d.setMinutes(parseInt(minutes))
+  d.setSeconds(parseInt(seconds) + 1)
+
+  return `${h}:${m}:${s}`
+}
+
+function activeClock() {
+  const activeClock = document.querySelector('[active-clock]')
+  if (!activeClock) return
+}
+
+activeClock()
