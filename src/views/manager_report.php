287a43b30228d43438961668e5cf125e2f974837
Felipe Moreira <104744113+FelipeM0reira@users.noreply.github.com>
2023-09-06 12:59:15 -0300

    feat: Consulas gerenciais.

--- src/views/manager_report.php
+++ src/views/manager_report.php
@@ -1,3 +1,9 @@
 <main class="content">
-  <h1>Ok</h1>
+  <?php
+    echo "Qtde de Usuários ativos: {$activeUsersCount}"; 
+    echo "<br>";
+    echo "Horas trabalhadas no mês: {$hoursInMonth}"; 
+    echo "<br>";
+    print_r($absentUsers);
+  ?>
 </main>
