80ecc68572bd5382ea1bb9b382a3743a28755398
Felipe Moreira <104744113+FelipeM0reira@users.noreply.github.com>
2023-09-18 20:21:52 -0300

    Update recover_password.php

--- src/controllers/recover_password.php
+++ src/controllers/recover_password.php
@@ -6,7 +6,7 @@ $exception = null;
 if(count($_POST) > 0) {
   $recover = new Login($_POST);
   try {
-    $recover->checkEmail();
+    $user = $recover->checkEmail();
   } catch (AppException $e) {
     $exception = $e;
   }
